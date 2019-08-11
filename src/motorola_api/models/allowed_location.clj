9ebfe4be4bd5c0f36e5a3543ea11c34a82ff3f51
Alex Ragone <ragonedk@gmail.com>
2019-08-11 16:27:35 +0200

    feat: added models

---
+++ src/motorola_api/models/allowed_location.clj
@@ -0,0 +1,4 @@
+(ns motorola-api.models.allowed-location
+  (:require [toucan.models :refer [defmodel]]))
+
+(defmodel AllowedLocation :allowed_location)
---
+++ src/motorola_api/models/radio.clj
@@ -0,0 +1,4 @@
+(ns motorola-api.models.radio
+  (:require [toucan.models :refer [defmodel]]))
+
+(defmodel Radio :radio)
